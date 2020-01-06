a2bf7c14e16d42aae486c1adb68e5610ef593267
kowframe <51714537+kowframe@users.noreply.github.com>
2020-01-06 17:55:35 +0700

    Update getHtmlToExcel.py
    change variable and update task in comment

--- getHtmlToExcel.py
+++ getHtmlToExcel.py
@@ -12,14 +12,17 @@ import pandas as pd
 from pandas import ExcelWriter
 from pandas import ExcelFile
 import numpy as np
-  
-# The webpage URL whose table we want to extract 
-url = "http://obecimso.net/web63/listStudentRegisR1.php?area_code=013"
+
+mainURL = 'http://obecimso.net/web63/listOfficeRoomR1.php'
+
+# Need for loop this URL 
+dataURL = "http://obecimso.net/web63/listStudentRegisR1.php?area_code=013"
   
 # Read data must use array[1]
-table = pd.read_html(url)[1] 
+table = pd.read_html(dataURL)[1] 
  
-writer = ExcelWriter('D:/python_sc/dataFromWeb.xlsx')
+# Need re-name excel with label from Website link (use variable with array insted fix String) 
+writer = ExcelWriter('D:/python_sc/Excel/dataFromWeb.xlsx')
 table.to_excel(writer, 'Sheet1', index=False)
 writer.save()
 
